efe21bfe94f6bb97a8fb2ab84d418fd1372ba099
Khushhall <kcm00000@gmail.com>
2016-09-15 04:16:06 +0530

    Added code.Checked corner cases

--- addition.cpp
+++ addition.cpp
@@ -1,1 +1,52 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void swap(string& str1, string& str2){
+	string temp = str1;
+	str1 = str2;
+	str2 = temp;
+	return;
+}
+
+string add(string str1,string str2){
+	int n = str1.length();
+	int m = str2.length();
+	string out = "";
+	int carry = 0;
+	if(n<m){
+		swap(str1,str2);
+		n = str1.length();
+		m = str2.length();
+	}
+
+	char temp;
+	while(m--){
+		n--;
+		int sum = carry + str1[n] + str2[m] - '0' -'0' ;
+		carry = sum/10;
+		sum = sum%10;
+		temp = sum + '0';
+		out = temp + out;
+	}
+
+	while(n--){
+		int sum = carry + str1[n] -'0' ;
+		carry = sum/10;
+		sum = sum%10;
+		temp = sum + '0';
+		out = temp + out;		
+	}
+
+	if(carry != 0){
+		temp = carry + '0';
+		out = temp + out;		
+	}	
+
+	return out;
+}
+
+// int main(){
+// 	cout<<add("1","999")<<endl;
+// 	return 0;
+// }
--- multiply.cpp
+++ multiply.cpp
@@ -1,12 +1,46 @@
-#incude <bits/stdc++.h>
+#include <bits/stdc++.h>
+#include "addition.cpp"
 
 using namespace std;
 
 string multiply(string str1,string str2){
-   string out;
-   return out;
+	int n = str1.length();
+	int m = str2.length();
+	if(n<m){
+		swap(str1,str2);
+		n = str1.length();
+		m = str2.length();
+	}
+
+	string out = "";
+	char temp;
+	
+	for(int j =m-1;j>=0;j--){
+		string tempStr = "";
+		int carry = 0;
+		for(int i =n-1;i>=0;i--){
+			int mul = ((str1[i] - '0') * (str2[j] -'0')) + carry ;
+			carry = mul/10;
+			mul = mul%10;
+			temp = mul + '0';
+			tempStr = temp + tempStr;
+		}
+
+		if(carry != 0){
+			temp = carry + '0';
+			tempStr = temp + tempStr;		
+		}		
+
+		if(j==m-1)
+			out = tempStr;
+		else
+			out = add(tempStr+'0',out);
+	}
+
+ 	return out;
 }
 
 int main(){
-    return 0;
+	cout<<multiply("98","999")<<endl;
+	return 0;
 }
